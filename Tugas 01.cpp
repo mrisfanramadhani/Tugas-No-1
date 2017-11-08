7902cf2e6d71969db2573bf83e547c5bb6b640ac
mrisfanramadhani <32300986+mrisfanramadhani@users.noreply.github.com>
2017-11-08 19:32:38 +0800

    Hasil

--- Tugas 01.cpp
+++ Tugas 01.cpp
@@ -4,4 +4,11 @@ int main(){
 	int X, Y, Z;
 	cout<<"Masukkan X\t\t= "; cin>>X;
 	cout<<"Masukkan Y\t\t= "; cin>>Y;
+	if(X<Y){
+			Z=Y-X;
+	}
+	else{
+			Z=X-Y;
+	}
+	cout<<"Hasil Pengurangan \t = "<<Z;
 }
